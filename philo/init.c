da9553cc9de65a93826169ac6048febe13800bd4
Timur Galyautdinov <tgalyaut@c2r14s2.42yerevan.am>
2023-08-04 18:04:50 +0400

    hi there

--- philo/init.c
+++ philo/init.c
@@ -1,4 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgalyaut <tgalyaut@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/04 16:05:56 by tgalyaut          #+#    #+#             */
+/*   Updated: 2023/08/04 16:59:16 by tgalyaut         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
+
+static void	ft_print(t_table *table)
+{
+	printf("philos: %d\n", table->philos);
+	printf("time_to_die: %ld\n", table->time_to_die);
+	printf("time_to_eat: %ld\n", table->time_to_eat);
+	printf("time_to_sleep: %ld\n", table->time_to_sleep);
+	printf("must_eat: %d\n", table->must_eat);
+}
+
+static void	ft_philo_init(t_table *table)
+{
+	unsigned int	i;
+
+	table->thread = malloc(sizeof(t_philo) * table->philos);
+	table->forks = malloc(sizeof(pthread_mutex_t) * table->philos);
+	i = 0;
+	while (i < table->philos)
+		pthread_mutex_init(table->forks + i++, 0);
+	i = 0;
+	while (i < table->philos)
+	{
+		table->thread[i].id = i + 1;
+		// printf("philo[%u]: %u\n", i, table->thread[i].id);
+		table->thread[i].times_ate = 0;
+		table->thread[i].fork1 = &table->forks[i];
+		// printf("fork1[%u]: %p\n", i, table->thread[i].fork1);
+		if (i != table->philos - 1)
+			table->thread[i].fork2 = &table->forks[i + 1];
+		else
+			table->thread[i].fork2 = &table->forks[0];
+		// printf("fork2[%u]: %p\n", i, table->thread[i].fork2);
+		++i;
+	}
+}
 
 t_table	*set_table(int ac, char **av, int i)
 {
@@ -14,5 +61,7 @@ t_table	*set_table(int ac, char **av, int i)
 	table->must_eat = -42;
 	if (ac - 1 == 5)
 		table->must_eat = ft_atoi(av[i]);
+	ft_philo_init(table);
+	ft_print(table);
 	return (table);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: tgalyaut <tgalyaut@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 20:27:37 by tgalyaut          #+#    #+#             */
-/*   Updated: 2023/07/28 20:27:38 by tgalyaut         ###   ########.fr       */
+/*   Updated: 2023/08/04 16:34:20 by tgalyaut         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,11 @@ typedef struct s_philo
 	pthread_t		thread;
 	unsigned int	id;
 	unsigned int	times_ate;
-	unsigned int	fork[2];
-	pthread_mutex_t	meal_time_lock;
+	pthread_mutex_t	*fork1;
+	pthread_mutex_t	*fork2;
+	pthread_mutex_t	*meal_time_lock;
 	time_t			last_meal;
-}				t_philo;
+}	t_philo;
 
 typedef struct s_table
 {
@@ -38,8 +39,9 @@ typedef struct s_table
 	time_t			time_to_sleep;
 	int				must_eat;
 	unsigned int	philos;
-	t_philo			**thread;
-}				t_table;
+	pthread_mutex_t	*forks;
+	t_philo			*thread;
+}	t_table;
 
 typedef enum e_condition
 {
@@ -49,7 +51,7 @@ typedef enum e_condition
 	THINKING = 3,
 	FORK_1 = 4,
 	FORK_2 = 5
-}				t_condition;
+}	t_condition;
 
 int	ft_parse(int ac, char **av);
 int	ft_atoi(char *str);
