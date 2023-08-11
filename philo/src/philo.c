c9aaa683b2180d889fbe0674dfc894788335f9ee
olnytim <yearagotomorrow@gmail.com>
2023-08-11 19:06:44 +0400

    philo

--- philo/src/philo.c
+++ philo/src/philo.c
@@ -12,6 +12,48 @@
 
 #include "../hf/philo.h"
 
+static void	ft_eat(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->fork1);
+	ft_print_status(philo, 0, 3);
+	pthread_mutex_lock(&philo->fork2);
+	ft_print_status(philo, 0, 3);
+	ft_print_status(philo, 0, 4);
+	pthread_mutex_lock(&philo->meal_lock);
+	philo->last_meal = ft_start_time();
+	pthread_mutex_unlock(&philo->meal_lock);
+	if (ft_the_end(philo->table) == 0)
+	{
+		pthread_mutex_lock(&philo->meal_lock);
+		philo->times_ate += 1;
+		pthread_mutex_unlock(&philo->meal_lock);
+	}
+	ft_print_status(philo, 0, 1);
+	pthread_mutex_unlock(&philo->fork1);
+	pthread_mutex_unlock(&philo->fork2);
+	ft_sleep(philo->table->time_to_sleep);
+}
+
+static void	ft_think(t_philo *philo, int mute)
+{
+	time_t	time_to_think;
+
+	pthread_mutex_lock(&philo->meal_lock);
+	time_to_think = (philo->table->time_to_die 
+		- (ft_start_time() - philo->last_meal) 
+		- philo->table->time_to_eat) / 2;
+	pthread_mutex_unlock(&philo->meal_lock);
+	if (time_to_think < 0)
+		time_to_think = 0;
+	if (time_to_think == 0 && mute == 1)
+		time_to_think = 1;
+	if (time_to_think > 600)
+		time_to_think = 200;
+	if (mute == 0)
+		ft_print_status(philo, 0, 2);
+	ft_sleep(time_to_think);
+}
+
 static void	*ft_one_philo(t_philo *philo)
 {
 	pthread_mutex_lock(philo->fork1);
@@ -36,5 +78,12 @@ void	*ft_philo(void *info)
 		return (NULL);
 	if (philo->table->philos == 1)
 		return (ft_one_philo(philo));
+	else if (philo->id - 1 % 2)
+		ft_think(philo, 1);
+	while (ft_the_end(philo->table) == 0)
+	{
+		ft_eat(philo);
+		ft_think(philo, 0);
+	}
 	return (NULL);
 }
